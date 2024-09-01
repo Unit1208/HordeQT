9ed8536f76e726b9aa2f8449d664a804f90f66c6
Unit1208 <unit1208@national.shitposting.agency>
2024-09-01 15:05:54 -0700

    WIP prompt library

---
+++ QTProj/QtHorde/prompt_library.py
@@ -0,0 +1,27 @@
+from typing import Optional
+from PySide6.QtCore import Qt
+from ui_prompt_library import Ui_Dialog as PromptLibraryWidget
+from ui_prompt_item import Ui_Form as PromptItemWidget
+
+# from PySide6.QtGui import
+from PySide6.QtWidgets import QDialog, QWidget
+
+
+class PromptItem(QWidget):
+    def __init__(self, parent: Optional[QWidget],name:str,value:str) -> None:
+        super().__init__(parent)
+        self.ui: PromptItemWidget = PromptItemWidget()
+        self.ui.setupUi(self)
+        self.ui.fieldLineEdit.setText(name)
+        self.ui.plainTextEdit.setPlainText(value)
+        
+
+class PromptLibrary(QDialog):
+    def __init__(self, parent: Optional[QWidget], f: Qt.WindowType) -> None:
+        super().__init__(parent, f)
+        self.ui: PromptLibraryWidget = PromptLibraryWidget()
+        self.ui.setupUi(self)
+
+    def add_item(self, ui_type: str, ui_value: str):
+        # self.ui.scrollAreaWidgetContents
+        self.ui.verticalLayout.addWidget(PromptItem(None,ui_type,ui_value))        
