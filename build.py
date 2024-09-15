116e29bd92e4cd235c45f259ff9580fe42deccae
Unit1208 <unit1208@national.shitposting.agency>
2024-09-14 17:06:31 -0700

    Update icon creation

--- build.py
+++ build.py
@@ -107,29 +107,6 @@ def install_sys_reqs():
     pass
 
 
-def convert_icon(icon_path, output_dir):
-    # Ensure the output directory exists
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    
-    # Load the original icon image
-    icon = Image.open(icon_path)
-    
-    # Define output sizes
-    sizes = [16, 32, 64, 128, 256, 512]
-    
-    # Convert to PNGs at different sizes
-    for size in sizes:
-        resized_icon = icon.resize((size, size), Image.Resampling.BICUBIC)
-        resized_icon.save(os.path.join(output_dir, f"icon_{size}.png"))
-    
-    # Convert to .icns (macOS icon format)
-    if icon.mode != 'RGBA':
-        icon = icon.convert("RGBA")  # Ensure the icon has an alpha channel
-    icon.save(os.path.join(output_dir, "icon.icns"), format='ICNS')
-    
-    # Convert to .ico (Windows icon format)
-    icon.save(os.path.join(output_dir, "icon.ico"), format='ICO', sizes=[(size, size) for size in sizes])
 
 def main():
     curr_dir = Path.cwd()
@@ -176,9 +153,9 @@ def main():
         formats.append("pkg")
     briefcase_exec = find_executable("briefcase", venv_path)
     
-    # Convert the icon to required formats and sizes
-    convert_icon("src/hordeqt/resources/Icon.png", "src/hordeqt/resources/icons")
-    subprocess.run([briefcase_exec, "dev", "--no-run", "-r"])
+
+    subprocess.run([briefcase_exec, "update", "--no-input", "-r","--update-resources"])
+    
     convert_uic_files(venv_path)
 
     for f in formats:
---
+++ scripts/create_icons.py
@@ -0,0 +1,39 @@
+import os
+import shutil
+import subprocess
+import sys
+from pathlib import Path
+from typing import List, Optional
+import platform
+import os
+from PIL import Image
+
+import os
+import sys
+from pathlib import Path
+import shutil
+def convert_icon(icon_path, output_dir):
+    # Ensure the output directory exists
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    
+    # Load the original icon image
+    icon = Image.open(icon_path)
+    
+    # Define output sizes
+    sizes = [16, 32, 64, 128, 256, 512]
+    
+    # Convert to PNGs at different sizes
+    for size in sizes:
+        resized_icon = icon.resize((size, size), Image.Resampling.BICUBIC)
+        resized_icon.save(os.path.join(output_dir, f"icon_{size}.png"))
+    
+    # Convert to .icns (macOS icon format)
+    if icon.mode != 'RGBA':
+        icon = icon.convert("RGBA")  # Ensure the icon has an alpha channel
+    icon.save(os.path.join(output_dir, "icon.icns"), format='ICNS')
+    
+    # Convert to .ico (Windows icon format)
+    icon.save(os.path.join(output_dir, "icon.ico"), format='ICO', sizes=[(size, size) for size in sizes])
+if __name__=="__main__":
+    convert_icon("../src/hordeqt/resources/Icon.png", "../src/hordeqt/resources/icons")
